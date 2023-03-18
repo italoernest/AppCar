3cf8f3d69a4f204b7b67d5f29091fad2d8ace18a
BPS-PCEscritorio <italoernest1@hotmail.com>
2023-03-18 00:54:12 -0300

    Atualização 18/03

---
+++ app/src/main/java/br/com/bpstelecom/appcar/UploadActivity.java
@@ -0,0 +1,14 @@
+package br.com.bpstelecom.appcar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+}
