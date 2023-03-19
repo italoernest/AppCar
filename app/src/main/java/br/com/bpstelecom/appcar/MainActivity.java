6b2c20d7dce194ceb73f4bf23f76eb9bbdbf9306
BPS-PCEscritorio <italoernest1@hotmail.com>
2023-03-19 14:06:44 -0300

    Atualização 19/03

--- app/src/main/java/br/com/bpstelecom/appcar/MainActivity.java
+++ app/src/main/java/br/com/bpstelecom/appcar/MainActivity.java
@@ -23,6 +23,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logout);
+        textView = findViewById(R.id.user_details);
+        user = auth.getCurrentUser();
+        if(user == null){
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivities(new Intent[]{intent});
+            finish();
+        }else{
+            textView.setText(user.getEmail());
+        }
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivities(new Intent[]{intent});
+                finish();
+            }
+        });
     }
 }
